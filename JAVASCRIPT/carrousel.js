8a47167f715ffbb40e48b735370e3d425611c6d0
Lucina <133715782+LucinaHiga@users.noreply.github.com>
2023-11-29 23:02:31 -0300

    ultimas correciones

--- JAVASCRIPT/carrousel.js
+++ JAVASCRIPT/carrousel.js
@@ -3,16 +3,12 @@
 const grande = document.querySelector('.grande')
 const punto = document.querySelectorAll ('.punto')
 
-//CUANDO CLICK en punto
-    //Saber la posicion de ese punto 
-    //Aplicar un transform translatex al grande
-    // QUITAR la clase activo de TODOS puntos
-    //Añadir la clase activo al punto que hemos hecho clik 
 
 
-//recorrer TODOS los puntos    
+
+  
 punto.forEach( (cadaPunto , i )=> {
-    //Asignamos un CLICK a cada punto
+    
     punto[i].addEventListener('click', ()=>{
 
         let posicion = i 
